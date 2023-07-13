446c88881298a2393539b60e0aeba0e32ac2819b
Emre <emreerdogdu76@gmail.com>
2023-07-13 18:04:20 +0300

    Atari Salonu

--- Assets/Scripts/Collide.cs
+++ Assets/Scripts/Collide.cs
@@ -1,30 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
 
 public class Collide : MonoBehaviour
 {
-    /*
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("UI"))
         {
-            var a GameObject = other.gameObject.transform.GetChild(0).gameObject;
-            a.SetActive(true);
 
+            var a = other.gameObject.transform.GetChild(0).gameObject;
+
+            a.SetActive(true);
+            
+            
+            //other. GetComponentInChildren<GameObject>().gameObject.SetActive(true);
             
 
-
         }
+        
+
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("UI"))
         {
-            var a GameObject = other.gameObject.transform.GetChild(0).gameObject;
+
+            var a = other.gameObject.transform.GetChild(0).gameObject;
+
             a.SetActive(false);
 
         }
     }
-    */
+  
 }
