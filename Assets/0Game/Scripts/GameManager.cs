c23d7584a17baae5be397315c9ce5665df896f1e
YusufSener <yusuf_sener_97@hotmail.com>
2023-07-17 01:39:33 +0300

    Add Yusuf Export

--- Assets/0Game/Scripts/GameManager.cs
+++ Assets/0Game/Scripts/GameManager.cs
@@ -7,12 +7,32 @@ namespace Emre
     public class GameManager : MonoBehaviour
     {
         [SerializeField] GameObject _startscreen, _game;
+        private void Start()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            if (PlayerPrefs.GetFloat("CheckScene") == 1)
+            {
+                _startscreen.SetActive(false);
+                _game.SetActive(true);
+            }
+        }
         public void GameStartButton()
         {
             _startscreen.SetActive(false);
             _game.SetActive(true);
+            PlayerPrefs.SetFloat("CheckScene", 1);
         }
-        public void QuitButton() => Application.Quit();
-        public void SceneUpload(int sceneIndex) => SceneManager.LoadScene(sceneIndex);
+        public void QuitButton()
+        {
+            PlayerPrefs.SetFloat("CheckScene", 0);
+            Application.Quit();
+        }
+        public void SceneUpload(int sceneIndex)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            SceneManager.LoadScene(sceneIndex);
+        }
     }
 }
