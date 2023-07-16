e7f1d5fce0572e730ab1643c4568aae91415d635
YusufSener <yusuf_sener_97@hotmail.com>
2023-07-16 23:05:57 +0300

    Add StartScreen

--- Assets/0Game/Scripts/Collide.cs
+++ Assets/0Game/Scripts/Collide.cs
@@ -1,41 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
-
-
-public class Collide : MonoBehaviour
+namespace Emre
 {
-    
-    private void OnTriggerEnter(Collider other)
+    public class Collide : MonoBehaviour
     {
-        if (other.gameObject.CompareTag("UI"))
+        private void OnTriggerEnter(Collider other)
         {
-
-            var a = other.gameObject.transform.GetChild(0).gameObject;
-
-            a.SetActive(true);
-            
-            
-            //other. GetComponentInChildren<GameObject>().gameObject.SetActive(true);
-            
-
+            if (other.gameObject.CompareTag("UI"))
+            {
+                var a = other.gameObject.transform.GetChild(0).gameObject;
+                a.SetActive(true);
+            }
         }
-        
-
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("UI"))
+        private void OnTriggerExit(Collider other)
         {
-
-            var a = other.gameObject.transform.GetChild(0).gameObject;
-
-            a.SetActive(false);
-
+            if (other.gameObject.CompareTag("UI"))
+            {
+                var a = other.gameObject.transform.GetChild(0).gameObject;
+                a.SetActive(false);
+            }
         }
     }
-  
 }
---
+++ Assets/0Game/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace Emre
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] GameObject _startscreen, _game;
+        public void GameStartButton()
+        {
+            _startscreen.SetActive(false);
+            _game.SetActive(true);
+        }
+        public void QuitButton()
+        {
+            Application.Quit();
+        }
+        public void SceneUpload(int sceneIndex)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+
+    }
+}
